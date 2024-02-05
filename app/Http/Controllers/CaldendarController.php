3f0cc287f5bf50395354c0c7e0b084043a55bd98
Arman <mdalarmansorker@gmail.com>
2024-02-05 14:27:27 +0600

    initial commit with mongodb installed

---
+++ app/Http/Controllers/CaldendarController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Caldendar;
+use Illuminate\Http\Request;
+
+class CaldendarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Caldendar  $caldendar
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Caldendar $caldendar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Caldendar  $caldendar
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Caldendar $caldendar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Caldendar  $caldendar
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Caldendar $caldendar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Caldendar  $caldendar
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Caldendar $caldendar)
+    {
+        //
+    }
+}
